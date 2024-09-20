d07cbd2b63d82743388261add8c29c1262e4ef15
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:26:33 -0400

    we get a 4 byte array, hooray

--- test/prototyping.f90
+++ test/prototyping.f90
@@ -1,8 +1,29 @@
-program prototyping
-  use :: fortran_vector
+module cool
+  use, intrinsic :: iso_c_binding
   implicit none
 
-  
+  type :: bloof
+    integer(c_int) :: x
+  end type bloof
+
+end module cool
+
+program prototyping
+  use :: cool
+  use :: fortran_vector
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  integer(c_int), target :: test
+  type(bloof), target :: dat
+
+  test = 1
+
+  ! call new_vector(c_loc(test))
+
+  dat%x = 2147483647
+
+  call new_vector(c_loc(dat), sizeof(dat))
 
 
 end program prototyping
