7d9c818529bc5dfb9ccc73a329eca5c217c31e70
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 10:55:45 -0400

    I'm sure this will segfault

--- src/vector.f90
+++ src/vector.f90
@@ -52,6 +52,7 @@ contains
   end subroutine vector_destroy
 
 
+  !* Get an element at an index in the vector.
   subroutine vector_at(this, index, generic_pointer)
     implicit none
 
@@ -70,6 +71,107 @@ contains
   end subroutine vector_at
 
 
+  !* Check if the vector is empty.
+  function vector_is_empty(this) result(empty)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    logical(c_bool) :: empty
+
+    empty = internal_vector_is_empty(this%data)
+  end function vector_is_empty
+
+
+  !* Get the number of elements in the vector.
+  function vector_size(this) result(size)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    integer(c_size_t) :: size
+
+    size = internal_vector_size(this%data)
+  end function vector_size
+
+
+  !* Get the total allocated size (in elements) of the vector.
+  !* You can think of this as: "slots available before a resize occurs"
+  function vector_capacity(this) result(cap)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    integer(c_size_t) :: cap
+
+    cap = internal_vector_capacity(this%data)
+  end function vector_capacity
+
+
+  !* Shrink the capacity of the vector to it's size.
+  !* (container size is trimmed to the current number of elements)
+  subroutine vector_shrink_to_fit(this)
+    implicit none
+
+    class(vec), intent(inout) :: this
+
+    call internal_vector_shrink_to_fit(this%data)
+  end subroutine vector_shrink_to_fit
+
+
+  !* Clear all the elements from the vector.
+  !* The GC function will run on each element.
+  subroutine vector_clear(this)
+    implicit none
+
+    class(vec), intent(inout) :: this
+
+    !! todo: needs to run the GC function here!
+
+    call internal_vector_clear(this%data)
+  end subroutine vector_clear
+
+
+  !* Insert an element into an index of the array.
+  subroutine vector_insert(this, index, generic)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    integer(c_size_t), intent(in), value :: index
+    class(*), intent(in), target :: generic
+    integer(1), dimension(:), pointer :: black_magic
+
+    !! Just WAT.
+
+    black_magic = transfer(generic, 1_1, size = this%size_of_type)
+
+    call internal_vector_insert(this%data, index, c_loc(black_magic), this%size_of_type)
+  end subroutine vector_insert
+
+
+  !* Erase an element from the vector at an index.
+  subroutine vector_erase(this, index)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    integer(c_size_t), intent(in), value :: index
+
+    !! todo: needs to run the GC function here!
+
+    call internal_vector_erase(this%data, index)
+  end subroutine vector_erase
+
+
+  !* Uses memcpy under the hood.
+  !* Push an element to the back of the vector.
+  subroutine vector_push_back(this, generic)
+    implicit none
+
+    class(vec), intent(inout) :: this
+    class(*), intent(in), target :: generic
+    integer(1), dimension(:), pointer :: black_magic
+
+    black_magic = transfer(generic, 1_1, size = this%size_of_type)
+
+    call internal_vector_push_back(this%data, c_loc(black_magic), this%size_of_type)
+  end subroutine vector_push_back
 
 
 
