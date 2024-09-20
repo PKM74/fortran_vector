cb8235246d315e8c3b4af332a958a5846574b4a4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:26:24 -0400

    going to need this

--- src/fortran_vector.f90
+++ src/fortran_vector.f90
@@ -1,4 +1,5 @@
 module fortran_vector
+  use, intrinsic :: iso_c_binding
   implicit none
 
   interface
@@ -13,5 +14,9 @@ module fortran_vector
 
 
   end interface
+
+contains
+
+
 
 end module fortran_vector
