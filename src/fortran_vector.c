67b4e2485fb92946d601403faa16d7cd700737f9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:05:41 -0400

    Put some truly random testing nonsense in

--- src/fortran_vector.c
+++ src/fortran_vector.c
@@ -1,2 +1,8 @@
-#include <cvector.h>
-#include <cvector_utils.h>
+#include <stdio.h>
+#include "cvector.h"
+#include "cvector_utils.h"
+
+void *new_vector(void *input_type)
+{
+  cvector_vector_type(typeof(input_type)) v = NULL;
+}
