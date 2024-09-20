5b853ccb2ef225012e2994a69b1c886528040191
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 09:40:32 -0400

    internalize

--- src/fortran_vector.f90
+++ src/fortran_vector.f90
@@ -4,34 +4,44 @@ module fortran_vector
 
   interface
 
-    ! todo: make these internal.
 
-    function new_vector(initial_size, element_size, gc_func) result(vec_pointer) bind(c, name = "new_vector")
+
+    function internal_new_vector(initial_size, element_size, gc_func) result(vec_pointer) bind(c, name = "new_vector")
       use, intrinsic :: iso_c_binding
       implicit none
 
       integer(c_size_t), intent(in), value :: initial_size, element_size
       type(c_funptr), intent(in), optional :: gc_func
       type(c_ptr) :: vec_pointer
-    end function new_vector
+    end function internal_new_vector
+
+    subroutine internal_destroy_vector(vec_pointer) bind(c, name = "destroy_vector")
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      type(c_ptr), intent(in), value :: vec_pointer
+    end subroutine internal_destroy_vector
+
+
 
 
     !* Uses memcpy under the hood.
-    subroutine push_back(vec_pointer, new_element_pointer, element_size) bind(c, name = "vector_push_back")
+    subroutine internal_push_back(vec_pointer, new_element_pointer, element_size) bind(c, name = "vector_push_back")
       use, intrinsic :: iso_c_binding
       implicit none
 
       type(c_ptr), intent(in), value :: vec_pointer, new_element_pointer
       integer(c_size_t), intent(in), value :: element_size
-    end subroutine push_back
+    end subroutine internal_push_back
 
-    function get_size(vec_pointer) result(vec_size) bind(c, name = "vector_size")
+    !* Get the size of a vector.
+    function internal_get_size(vec_pointer) result(vec_size) bind(c, name = "vector_size")
       use, intrinsic :: iso_c_binding
       implicit none
 
       type(c_ptr), intent(in), value :: vec_pointer
       integer(c_size_t) :: vec_size
-    end function get_size
+    end function internal_get_size
 
 
   end interface
