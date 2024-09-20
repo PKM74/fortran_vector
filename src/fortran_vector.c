161b0f12cb511101f1279ece27f95d5e85866ea4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:19:56 -0400

    In other news, local man goes insane

--- src/fortran_vector.c
+++ src/fortran_vector.c
@@ -2,7 +2,19 @@
 #include "cvector.h"
 #include "cvector_utils.h"
 
-void *new_vector(void *input_type)
+void *new_vector(void *input_type, size_t data_size)
 {
-  cvector_vector_type(typeof(input_type)) v = NULL;
+
+  // This is what insanity looks like.
+  size_t florf[data_size];
+  memcpy(&florf, input_type, data_size);
+
+  // printf("data size: %d\n", data_size);
+  // for (int i = 0; i < data_size; i++)
+  // {
+  //   printf("%d ", florf[i]);
+  // }
+  // printf("\n");
+
+  cvector_vector_type(typeof(florf)) v = NULL;
 }
