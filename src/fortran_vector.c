76fa63011888cde6c0e208149fd535f1e75a2ee5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:41:07 -0400

    This makes more sense

--- src/fortran_vector.c
+++ src/fortran_vector.c
@@ -3,19 +3,34 @@
 #include "cvector.h"
 #include "cvector_utils.h"
 
-void *new_vector(void *input_type, size_t data_size)
+/**
+ * @param data_size The size of the element you are using this vector for.
+ */
+void *new_vector(size_t data_size)
 {
 
   // This is what insanity looks like.
   uint8_t raw_data[data_size];
-  memcpy(&raw_data, input_type, data_size);
+  // memcpy(&raw_data, t, data_size);
 
-  printf("data size: %d\n", data_size);
-  for (int i = 0; i < data_size; i++)
-  {
-    printf("%d ", raw_data[i]);
-  }
-  printf("\n");
+  // printf("data size: %d\n", data_size);
+  // for (int i = 0; i < data_size; i++)
+  // {
+  //   printf("%d ", raw_data[i]);
+  // }
+  // printf("\n");
 
   cvector_vector_type(typeof(raw_data)) v = NULL;
+
+  return v;
+}
+
+void push_back(void *vec, void *new_element, size_t data_size)
+{
+  uint8_t raw_data[data_size];
+  memcpy(&raw_data, new_element, data_size);
+
+  cvector(void) *test = vec;
+
+  cvector_push_back(test, raw_data);
 }
