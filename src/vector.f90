de87ac29ddcf8e84a922c3bd4b3330798569fbdc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 10:17:38 -0400

    we gonna fly today

--- src/vector.f90
+++ src/vector.f90
@@ -13,16 +13,25 @@ module vector
   type :: vec
     private
     type(c_ptr) :: data
+    integer(c_size_t) :: size_of_type
   end type vec
+
 
 contains
 
-  function new_vec() result(v)
+
+  !* Create a new vector.
+  function new_vec(size_of_type, initial_size) result(v)
     implicit none
 
+    ! size_of_type allows you to simply get the size of your type before and hard code it into your program. 8)
+    integer(c_size_t), intent(in), value :: size_of_type, initial_size
     type(vec) :: v
 
-    
+    ! todo: needs a GC func thing.
+
+    v%data = internal_new_vector(initial_size, size_of_type, c_null_funptr)
+    v%size_of_type = size_of_type
   end function new_vec
 
 
