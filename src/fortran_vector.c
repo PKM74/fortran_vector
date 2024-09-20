f6ae55bf39c7f03f4b2123faba4189ddafb07ba7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:26:19 -0400

    stop being dumb

--- src/fortran_vector.c
+++ src/fortran_vector.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include "cvector.h"
 #include "cvector_utils.h"
 
@@ -6,15 +7,15 @@ void *new_vector(void *input_type, size_t data_size)
 {
 
   // This is what insanity looks like.
-  size_t raw_data[data_size];
+  uint8_t raw_data[data_size];
   memcpy(&raw_data, input_type, data_size);
 
-  // printf("data size: %d\n", data_size);
-  // for (int i = 0; i < data_size; i++)
-  // {
-  //   printf("%d ", florf[i]);
-  // }
-  // printf("\n");
+  printf("data size: %d\n", data_size);
+  for (int i = 0; i < data_size; i++)
+  {
+    printf("%d ", raw_data[i]);
+  }
+  printf("\n");
 
   cvector_vector_type(typeof(raw_data)) v = NULL;
 }
