3a37411eefe8f6a594d257916ee62471a06adbdf
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-20 08:05:47 -0400

    And wrapper it

--- src/fortran_vector.f90
+++ src/fortran_vector.f90
@@ -1,10 +1,16 @@
 module fortran_vector
   implicit none
-  private
 
-  public :: say_hello
-contains
-  subroutine say_hello
-    print *, "Hello, fortran_vector!"
-  end subroutine say_hello
+  interface
+
+    subroutine new_vector(t) bind(c, name = "new_vector")
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      type(c_ptr), intent(in), value :: t
+    end subroutine new_vector
+
+
+  end interface
+
 end module fortran_vector
