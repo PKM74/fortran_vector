fd5b6c012cbb36150d0d35117e179ee64b145716
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-21 06:15:27 -0400

    swapped it back out

--- test/prototyping.f90
+++ test/prototyping.f90
@@ -40,76 +40,76 @@ program prototyping
 
   call v%reserve(3_8)
 
-  do
+  ! do
 
-    call v%push_back(1)
-    call v%push_back(2)
-    call v%push_back(3)
-    call v%push_back(4)
+  !   call v%push_back(1)
+  !   call v%push_back(2)
+  !   call v%push_back(3)
+  !   call v%push_back(4)
 
-    call v%erase(2_8)
+  !   call v%erase(2_8)
 
-    call c_f_pointer(v%at(1_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(1_8), output)
+  !   print*,"output:", output
 
-    call c_f_pointer(v%at(2_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(2_8), output)
+  !   print*,"output:", output
 
-    call c_f_pointer(v%at(3_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(3_8), output)
+  !   print*,"output:", output
 
-    print*,"begin insertion"
+  !   print*,"begin insertion"
 
-    call v%insert(2_8, 2)
+  !   call v%insert(2_8, 2)
 
-    call c_f_pointer(v%at(1_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(1_8), output)
+  !   print*,"output:", output
 
-    call c_f_pointer(v%at(2_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(2_8), output)
+  !   print*,"output:", output
 
-    call c_f_pointer(v%at(3_8), output)
-    print*,"output:", output
+  !   call c_f_pointer(v%at(3_8), output)
+  !   print*,"output:", output
 
-    call v%pop_back()
-    call v%pop_back()
-    call v%pop_back()
-    call v%pop_back()
-    call v%pop_back()
+  !   call v%pop_back()
+  !   call v%pop_back()
+  !   call v%pop_back()
+  !   call v%pop_back()
+  !   call v%pop_back()
 
-  end do
-
-
-
-  ! do i = 1,10000000
-
-  !   ! print*,i
-
-  !   ! i = i + 1
-
-  !   call v%push_back(i)
-
-  !   if (mod(v%size(), 1000000) == 0) then
-  !     print*,"new size: ",v%size()
-  !   end if
-
-  !   !? Insurance
-  !   call c_f_pointer(v%at(int(i, c_size_t)), output)
-  !   if (output /= i) then
-  !     error stop
-  !   end if
-
-  !   ! print*,"fortan output", output
-  !   ! call c_f_pointer(v%at(1_8), output)
-  !   ! print*,"fortan output", output
-  !   ! call c_f_pointer(v%at(2_8), output)
-  !   ! print*,"fortan output", output
-  !   ! call c_f_pointer(v%at(3_8), output)
-  !   ! print*,"fortan output", output
   ! end do
 
 
-  ! print*,"hi from fortran"
+
+  do i = 1,10000000
+
+    ! print*,i
+
+    ! i = i + 1
+
+    call v%push_back(i)
+
+    if (mod(v%size(), 1000000) == 0) then
+      print*,"new size: ",v%size()
+    end if
+
+    !? Insurance
+    call c_f_pointer(v%at(int(i, c_size_t)), output)
+    if (output /= i) then
+      error stop
+    end if
+
+    ! print*,"fortan output", output
+    ! call c_f_pointer(v%at(1_8), output)
+    ! print*,"fortan output", output
+    ! call c_f_pointer(v%at(2_8), output)
+    ! print*,"fortan output", output
+    ! call c_f_pointer(v%at(3_8), output)
+    ! print*,"fortan output", output
+  end do
+
+
+  print*,"hi from fortran"
 
   ! call sleep(10)
 
