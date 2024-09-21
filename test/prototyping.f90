f5a1f4d5ec0b7558606ac5c62e69fc1da19f3b52
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-21 06:37:03 -0400

    According to valgrind, everything is okay

--- test/prototyping.f90
+++ test/prototyping.f90
@@ -30,28 +30,49 @@ program prototyping
   type(bloof) :: dat
   type(vec) :: v
   type(c_ptr) :: generic_c_ptr
-  integer(c_int) :: i
+  integer(c_int) :: i, y
   type(bloof), pointer :: output
+  logical(c_bool) :: t
 
-  v = new_vec(sizeof(dat), int(0, c_size_t))
+  t = .true.
 
-  do i = 1,10000000
-    dat%x = i
-    dat%d = "hello"
-    allocate(character(22) :: dat%f)
-    dat%f = "foop"
-    call v%push_back(dat)
+  do y = 1,10
+    if (t) then
+      print*,"tick"
+    else
+      print*,"tock"
+    end if
+    t = .not. t
+
+    v = new_vec(sizeof(dat), int(10000000, c_size_t))
+
+    do i = 1,10000000
+      dat%x = i
+      dat%d = "hello"
+      allocate(character(22) :: dat%f)
+      dat%f = "foop"
+      call v%push_back(dat)
+    end do
+
+
+    do i = 1,10000000
+      generic_c_ptr = v%at(int(i, c_size_t))
+      call c_f_pointer(generic_c_ptr, output)
+      ! print*,output%x, output%d, output%f
+      deallocate(output%f)
+    end do
+
+
+    ! print*,"zzzz"
+    ! call sleep(3)
+    ! print*,"here we go!"
+
+    ! call v%clear()
+
+    call v%destroy()
+
+    ! print*,"zzzz (again)"
+    ! call sleep(3)
   end do
-
-
-  do i = 1,10000000
-    generic_c_ptr = v%at(int(i, c_size_t))
-    call c_f_pointer(generic_c_ptr, output)
-    print*,output%x, output%d, output%f
-    deallocate(output%f)
-  end do
-
-
-
 
 end program prototyping
